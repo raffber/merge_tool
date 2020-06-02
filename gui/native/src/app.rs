2695799a7cead956912a597fc1faa38cc38937ae
Raphael Bernhard <beraphae@gmail.com>
2020-06-02 10:07:02 +0200

    Start hooking up events

--- gui/native/src/app.rs
+++ gui/native/src/app.rs
@@ -1,38 +1,61 @@
 use greenhorn::prelude::*;
+use greenhorn::components::{checkbox, TextInput, TextInputMsg};
+use greenhorn::dialog::{FileOpenDialog, FileOpenMsg, FileFilter};
 
-pub enum MainMsg {
-    SayHello,
-    SayGoodbye,    
+pub enum Msg {
+    OpenConfig,
+    ConfigOpened(FileOpenMsg),
+    ConfigPathChanged(TextInputMsg),
 }
 
 pub struct MainApp {
-    msg: String,
+    product_name: TextInput,
+    config_path: TextInput,
 }
 
 impl MainApp {
     pub fn new() -> Self {
         Self {
-            msg: "something".into(),
+            product_name: Default::default(),
+            config_path: Default::default()
         }
     }
 }
 
+impl App for MainApp {
+    fn update(&mut self, msg: Self::Message, ctx: Context<Self::Message>) -> Updated {
+        match msg {
+            Msg::OpenConfig => {
+                let dialog = FileOpenDialog::new("Open a config file", "~")
+                    .with_filter(FileFilter::new("GCTBtl Config files").push("gctmrg"));
+                ctx.dialog(dialog, Msg::ConfigOpened)
+            },
+
+            Msg::ConfigOpened(msg) => {
+                println!("{:?}", msg)
+            },
+            Msg::ConfigPathChanged(msg) => {}
+        }
+        Updated::yes()
+    }
+}
+
 impl Render for MainApp {
-    type Message = MainMsg;
+    type Message = Msg;
 
     fn render(&self) -> Node<Self::Message> {
         use greenhorn::html;
 
         html!(
             <div .main-app .container-fluid>
-                <h1>{"Merge & Release Tool"}</>
                 <div class="row align-items-center my-2">
                     <div class="custom-control custom-switch mx-1 col-auto">
                         <input type="checkbox" class="custom-control-input" id="auto-save-toggle" />
                         <label class="custom-control-label" for="auto-save-toggle">{"Auto Save"}</>
                     </>
-                    <input type="text" class="col mx-1 form-control" placeholder="Path to config file..." />
-                    <button type="button" class="btn btn-secondary mx-1 col-auto">{"Open"}</>
+                    {self.config_path.render(Msg::ConfigPathChanged).class("col mx-1 form-control")
+                        .attr("placeholder", "Path to config file...")}
+                    <button type="button" class="btn btn-secondary mx-1 col-auto" @click={|_| Msg::OpenConfig}>{"Open"}</>
                 </>
                 <div class="row align-items-center my-2">
                     <span class="col-3">{"Product ID"}</>
@@ -43,9 +66,9 @@ impl Render for MainApp {
                 <div class="row align-items-center my-2">
                     <span class="col-3">{"State Transition Time"}</>
                     <input type="text" class="col-3 form-control" placeholder="in ms"/>
-                    <div class="col-6 form-check">
-                        <input type="checkbox" class="form-check-input" id="use-backdoor" />
-                        <label class="form-check-label" for="use-backdoor">
+                    <div class="col-6 px-5 custom-control custom-checkbox">
+                        <input type="checkbox" class="custom-control-input" id="use-backdoor" />
+                        <label class="custom-control-label" for="use-backdoor">
                         {"Use Backdoor"}
                         </>
                     </>                    
@@ -58,14 +81,4 @@ impl Render for MainApp {
             </>
         ).into()
     }
-}
-
-impl App for MainApp {
-    fn update(&mut self, msg: Self::Message, _ctx: Context<Self::Message>) -> Updated {
-        match msg {
-            MainMsg::SayHello => self.msg = "hello".into(),
-            MainMsg::SayGoodbye => self.msg = "goodbye".into()
-        }
-        Updated::yes()
-    }    
 }
