6238a18df54bbe5b6a460a5afd589886900e4ee8
Raphael Bernhard <beraphae@gmail.com>
2020-06-05 09:48:04 +0200

    start implementing fw_config image pane

--- gui/native/src/fw_config.rs
+++ gui/native/src/fw_config.rs
@@ -1,6 +1,7 @@
 use greenhorn::prelude::*;
 use greenhorn::html;
 use merge_tool::config::FwConfig;
+use crate::text_field::TextField;
 
 #[derive(Debug)]
 pub enum FwMsg {
@@ -12,7 +13,11 @@ pub enum FwMsg {
 pub struct FwPane {
     config: FwConfig,
     pub updated: Event<FwConfig>,
-    pub remove: Event<()>
+    pub remove: Event<()>,
+    fw_id: TextField<u16>,
+    btl_path: TextField<String>,
+    app_path: TextField<String>,
+
 }
 
 impl FwPane {
@@ -40,6 +45,18 @@ impl Render for FwPane {
     type Message = FwMsg;
 
     fn render(&self) -> Node<Self::Message> {
-        html!( <div #fw-config>{"Foobar uaahh!!!!"}</> ).into()
+        html!( <div #fw-config >
+                // row with product ID + Product name
+                <div class="d-flex flex-row align-items-center my-2">
+                    <span class="col-6">{"Firmware ID"}</>
+                    {self.fw_id.render().build().map(Msg::ProductIdMsg)}
+                </>
+                    {self.product_id.change_event().subscribe(Msg::ProductIdChanged)}
+                    <span class="col-3">{"Product Name"}</>
+                    {self.product_name.render().build().map(Msg::ProductNameMsg)}
+                    {self.product_name.change_event().subscribe(Msg::ProductNameChanged)}
+                </>
+
+        </> ).into()
     }
 }
