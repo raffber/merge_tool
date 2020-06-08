b1962a61309d14cba8a8b9d064d0333d64ba6782
Raphael Bernhard <beraphae@gmail.com>
2020-06-08 22:23:54 +0200

    gui: Log information where output files were written to

--- gui/native/src/runner.rs
+++ gui/native/src/runner.rs
@@ -29,16 +29,23 @@ pub fn generate_script(mut config: Config, config_path: &Path) -> impl Stream<It
                 return;
             }
         };
-        let folderpath = config_dir.join("out");
-        if let Err(_) = create_dir_all(&folderpath) {
+        let output_dir = config_dir.join("out");
+        if let Err(_) = create_dir_all(&output_dir) {
             tx.unbounded_send(RunnerMsg::Failure(
                 "Cannot create output directory!".to_string(),
             ))
             .unwrap();
             return;
         }
-        let msg = match process::create_script(&mut config, &config_dir, &folderpath) {
-            Ok(_) => RunnerMsg::Success("Successfully create script file!".to_string()),
+        let msg = match process::create_script(&mut config, &config_dir, &output_dir) {
+            Ok(_) => {
+                tx.unbounded_send(RunnerMsg::Info(format!(
+                    "Files written to `{}`",
+                    output_dir.to_str().unwrap()
+                )))
+                .unwrap();
+                RunnerMsg::Success("Successfully create script file!".to_string())
+            }
             Err(err) => RunnerMsg::Failure(err.to_string()),
         };
         tx.unbounded_send(msg).unwrap();
@@ -71,6 +78,11 @@ pub fn release(config: Config, config_path: &Path) -> impl Stream<Item = RunnerM
         let mut config = config;
         match process::release(&mut config, &config_dir, &target_dir) {
             Ok(_) => {
+                tx.unbounded_send(RunnerMsg::Info(format!(
+                    "Files written to `{}`",
+                    target_dir.to_str().unwrap()
+                )))
+                .unwrap();
                 tx.unbounded_send(RunnerMsg::Success(
                     "Successfully released firmware!".to_string(),
                 ))
@@ -92,6 +104,7 @@ fn do_merge(
     config: &mut Config,
     config_dir: &Path,
     target_dir: &Path,
+    tx: &mpsc::UnboundedSender<RunnerMsg>,
 ) -> Result<RunnerMsg, RunnerMsg> {
     create_dir_all(&target_dir)
         .map_err(|_| RunnerMsg::Failure("Cannot create output directory!".to_string()))?;
@@ -99,6 +112,11 @@ fn do_merge(
         .map_err(|err| RunnerMsg::Failure(format!("Cannot merge firmware images: {}", err)))?;
     process::write_fws(config, &fws, target_dir)
         .map_err(|err| RunnerMsg::Failure(format!("Cannot write firmware images: {}", err)))?;
+    tx.unbounded_send(RunnerMsg::Info(format!(
+        "Files written to `{}`",
+        target_dir.to_str().unwrap()
+    )))
+    .unwrap();
     Ok(RunnerMsg::Success(
         "Successfully merged firmware files!".to_string(),
     ))
@@ -120,7 +138,7 @@ pub fn merge(config: Config, config_path: &Path) -> impl Stream<Item = RunnerMsg
         };
         let target_dir = config_dir.join("out");
         let mut config = config;
-        let msg = match do_merge(&mut config, &config_dir, &target_dir) {
+        let msg = match do_merge(&mut config, &config_dir, &target_dir, &tx) {
             Ok(msg) => msg,
             Err(msg) => msg,
         };
