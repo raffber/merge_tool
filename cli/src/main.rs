ba6b5b0ec750bb1f041aeccf7e490a8a0e202255
Raphael Bernhard <beraphae@gmail.com>
2020-06-26 00:13:32 +0200

    cli: Fix creation of output directory

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,8 +1,10 @@
+use std::fs::create_dir_all;
+use std::path::{Path, PathBuf};
+
 use clap::{App, Arg, SubCommand};
+
 use merge_tool::config::Config;
 use merge_tool::process;
-use std::fs::{canonicalize, create_dir_all};
-use std::path::{Path, PathBuf};
 
 fn main() {
     let matches = App::new("Merge Tool")
@@ -51,17 +53,6 @@ fn main() {
         .value_of("output-dir")
         .map(PathBuf::from)
         .unwrap_or(config_dir.join("out"));
-    let output_dir = match canonicalize(output_dir) {
-        Ok(x) => x,
-        Err(err) => {
-            println!("Invalid output directory: {}", err);
-            return;
-        }
-    };
-    if let Err(err) = create_dir_all(&output_dir) {
-        println!("Cannot create output directory: {}", err);
-        return;
-    }
     let mut config = match Config::load_from_file(&config_path) {
         Ok(config) => config,
         Err(err) => {
@@ -71,12 +62,20 @@ fn main() {
     };
 
     if let Some(_) = matches.subcommand_matches("script") {
+        if let Err(err) = create_dir_all(&output_dir) {
+            println!("Cannot create output directory: {}", err);
+            return;
+        }
         if let Err(err) = process::create_script(&mut config, &config_dir, &output_dir) {
             println!("Error: Could not create bootload script: {}", err);
         }
     }
 
     if let Some(_) = matches.subcommand_matches("merge") {
+        if let Err(err) = create_dir_all(&output_dir) {
+            println!("Cannot create output directory: {}", err);
+            return;
+        }
         match process::merge_all(&mut config, &config_dir) {
             Ok(fws) => {
                 if let Err(err) = process::write_fws(&config, &fws, &output_dir) {
